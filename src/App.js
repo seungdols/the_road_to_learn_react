a9a3713769d5ed4afafe309f2ff5237608729b23
seungdols <seungdols0822@gmail.com>
2018-04-24 19:23:44 +0900

    1.6 JSX 기초 - JSX 새변수 만들기

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var h2Text = 'Hello, React World';
+    var user = {
+      firstName: 'seungdols',
+      lastName: 'choi'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{h2Text}</h2>
+        <p>user.firstName: {user.firstName}</p>
+        <p>user.lastName: {user.lastName}</p>
       </div>
     );
   }
