d0c7f82dafb781d0934deb664838c1a290d20e61
seungdols <seungdols0822@gmail.com>
2018-04-24 19:24:25 +0900

    1.6 JSX 기초 - JSX 새변수 만들기 세미 콜론 제거

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 
 class App extends Component {
   render() {
-    var h2Text = 'Hello, React World';
+    var h2Text = 'Hello, React World'
     var user = {
       firstName: 'seungdols',
       lastName: 'choi'
-    };
+    }
     return (
       <div className="App">
         <h2>{h2Text}</h2>
         <p>user.firstName: {user.firstName}</p>
         <p>user.lastName: {user.lastName}</p>
       </div>
-    );
+    )
   }
 }
 
