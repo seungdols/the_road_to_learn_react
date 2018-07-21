8e5a90953c8d6b37e27eb37e3185c554f4be3c9d
seungdols <seungdols0822@gmail.com>
2018-07-22 01:45:56 +0900

    2.8 제어되는 컴포넌트

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
     return (
       <div className="App">
         <form>
-          <input type="text" onChange={this.onSearchChange}/>
+          <input type="text" value={searchTerm} onChange={this.onSearchChange}/>
         </form>
 
         {list.filter(isSearched(searchTerm)).map(item =>
