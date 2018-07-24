a95ca88e6b44b669aad76aa9908bff51b99db43a
seungdols <seungdols0822@gmail.com>
2018-07-24 17:18:57 +0900

    3.6  페이지 매김 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -3,66 +3,81 @@ import logo from './logo.svg';
 import './App.css';
 import Search from './Search.js'
 import Table from './Table.js'
+import Button from './Button';
 
 const DEFAULT_QUERY = 'redux'
+const DEFAULT_HPP = '100'
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search'
 const PARAM_SEARCH = 'query='
+const PARAM_PAGE = 'page='
+const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       result: null,
       searchTerm: DEFAULT_QUERY
     };
 
     this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
     this.onSearchChange = this.onSearchChange.bind(this)
     this.onSearchSubmit = this.onSearchSubmit.bind(this)
     this.onDismiss = this.onDismiss.bind(this)
   }
 
-  setSearchTopStories(result) {
-    this.setState({result})
-  }
-
-
-  componentDidMount() {
+  componentDidMount () {
     const { searchTerm } = this.state
     this.fetchSearchTopStories(searchTerm)
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  setSearchTopStories (result) {
+    const { hits, page } = result
+    const oldHits = page !== 0 ? this.state.result.hits : []
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ]
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    })
+  }
+
+  fetchSearchTopStories (searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error)
   }
 
-  onDismiss(id) {
+  onDismiss (id) {
     const isNotId = item => item.objectID !== id
     const updatedHits = this.state.result.hits.filter(isNotId)
     this.setState({
-      result: {...this.state.result, hits: updatedHits }
+      result: { ...this.state.result, hits: updatedHits }
     });
   }
 
-  onSearchChange(event) {
+  onSearchChange (event) {
     this.setState({searchTerm: event.target.value})
   }
 
-  onSearchSubmit(event) {
-      const { searchTerm } = this.state
-      this.fetchSearchTopStories(searchTerm)
-      event.preventDefault()
+  onSearchSubmit (event) {
+    const { searchTerm } = this.state
+    this.fetchSearchTopStories(searchTerm)
+    event.preventDefault()
   }
 
-  render() {
+  render () {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0
 
-    if(!result) {
+    if (!result) {
       return null
     }
 
@@ -71,11 +86,15 @@ class App extends Component {
         <div className="interactions">
           <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>Search</Search>
         </div>
-          { result && <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} /> }
+        { result && <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} /> }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
+
     )
   }
 }
-
-
 export default App;
