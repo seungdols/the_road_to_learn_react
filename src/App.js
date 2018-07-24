e7d6188049fc6c0eb296a3beb385f947faf36813
seungdols <seungdols0822@gmail.com>
2018-07-24 11:30:23 +0900

    3.3 ES6 전개연산자

--- src/App.js
+++ src/App.js
@@ -36,22 +36,11 @@ class App extends Component {
   }
 
   onDismiss(id) {
-
-    // const updatedList = this.state.list.filter(function isNotId() {
-    //   return item.objectID !== id;
-    // });
-
-    // function isNotId(id) {
-    //   return item.objectID !== id;
-    // }
-
-    // const updatedList = this.state.list.filter(isNotId);
-
-    const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-
-    this.setState({list: updatedList});
-    console.log(this);
+    const isNotId = item => item.objectID !== id
+    const updatedHits = this.state.result.hits.filter(isNotId)
+    this.setState({
+      result: {...this.state.result, hits: updatedHits }
+    });
   }
 
   onSearchChange(event) {
