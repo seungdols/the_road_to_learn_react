e22127c2897e569c9a3211d7135bffd80c14580f
seungdols <seungdols0822@gmail.com>
2018-07-25 17:50:27 +0900

    3.9 Axios 라이브러리 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 import Search from './Search.js'
@@ -15,6 +16,8 @@ const PARAM_PAGE = 'page='
 const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
+  _isMounted = false
+
   constructor (props) {
     super(props)
     this.state = {
@@ -33,9 +36,14 @@ class App extends Component {
   }
 
   componentDidMount () {
+    this._isMounted = true
     const { searchTerm } = this.state
     this.setState({ searchKey: searchTerm })
     this.fetchSearchTopStories(searchTerm)
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
   }
 
   needsToSearchTopStories (searchTerm) {
@@ -59,10 +67,9 @@ class App extends Component {
   }
 
   fetchSearchTopStories (searchTerm, page = 0) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => this.setState({ error }))
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }))
   }
 
   onDismiss (id) {
